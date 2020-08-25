62f9353f4f3ff782ca0d42c6268b92aeb000db16
BryanSalgado <bryan.salgado.cl>
2020-08-24 23:40:29 -0400

    Commit 1

---
+++ lab3-204030790-salgado/src/lab3/pkg204030790/salgado/Lab3204030790Salgado.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3.pkg204030790.salgado;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Lab3204030790Salgado {
+
+    /**
+     * @param args the command line arguments
+     * El TDA que se aplicará consiste en una diversar clases que representarán las distintas zonas y commmits
+     * Existirán 4 clases que representarán las zonas: Workspace, Index, Local Repository y Remote Repository
+     * Las clases Local Repository y Remote Repository tendrán acceso a una clase extra llamada Commit
+     * Las clases Workspace, Index y Commit tendrán acceso a una clase llamada archivo.
+     * Esta última será la contendrá el texto plano en  forma de string
+     * Por último existirá una clase llamada Repositorio, está contendra las cuatro zonas
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Pastel de chocolate y pera");
+    }
+    
+}
+ 
